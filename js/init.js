cef87949616dd1ae82c19ca7193f24d4f668dba9
wpasumul <82428541+wpasumul@users.noreply.github.com>
2021-04-12 22:25:18 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// @ts-ignore
+// Your web app's Firebase configuration
+// Initialize Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyCx6uZlRAd2OjVPgLspc2MsSeLdrIkQ5H4",
+    authDomain: "wpasimul-92f82.firebaseapp.com",
+    projectId: "wpasimul-92f82",
+    storageBucket: "wpasimul-92f82.appspot.com",
+    messagingSenderId: "659355086391",
+    appId: "1:659355086391:web:63b2bc65a1baea77f219b2"
+});
